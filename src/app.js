47a87c3538e16197758797f622bb921db6105105
Enrique A <emoralesdelosrios@icloud.com>
2025-11-21 18:02:28 +0000

    generador de cartas

--- src/app.js
+++ src/app.js
@@ -5,6 +5,42 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+// variables
+const palo = [`♦`, `♥`, `♠`, `♣`]
+const valorCard = [`A`, `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, `10`, `J`, `Q`, `K`]
+const color = [`text-danger`, `text-dark`]
+const firstDiv = document.querySelector(`div`)
+const topNumber = document.createElement(`div`)
+const midNumber = document.createElement(`div`)
+const botNumber = document.createElement(`div`)
+
+
+// funciones
+const randomNumber = arr => arr[Math.floor(Math.random()*arr.length)]
+console.log(randomNumber(valorCard))
+
+
+
+
+//Eventos
+firstDiv.id = `card`;
+
+topNumber.id = `topNumber`
+topNumber.classList = randomNumber(color)
+topNumber.innerHTML = randomNumber(palo);
+firstDiv.appendChild(topNumber)
+
+midNumber.id = `midNumber`
+midNumber.innerHTML = randomNumber(valorCard);
+firstDiv.appendChild(midNumber)
+
+botNumber.id = `botNumber`
+botNumber.innerHTML = topNumber.innerHTML
+botNumber.classList = topNumber.classList
+firstDiv.appendChild(botNumber)
+
+
+
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
